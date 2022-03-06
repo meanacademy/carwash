141c577c7d32d336d25089b7269a2218997b363f
rushik7 <rushikeshk7@gmail.com>
2022-03-06 22:58:50 +0530

    git commit -m "carwash commit"

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
+import { BannerComponent } from './components/banner/banner.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    BannerComponent,
   ],
   imports: [
     BrowserModule
