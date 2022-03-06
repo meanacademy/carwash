44d4b337bac05676ed0d44cf4193c5a20991165c
rushik7 <rushikeshk7@gmail.com>
2022-03-06 23:45:43 +0530

    git push

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,9 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { BannerComponent } from './components/banner/banner.component';
+import { AboutusComponent } from './components/aboutus/aboutus.component';
+import { ServicesComponent } from './components/services/services.component';
+import { BookserviceComponent } from './components/bookservice/bookservice.component';
 
 @NgModule({
   declarations: [
@@ -12,6 +15,9 @@ import { BannerComponent } from './components/banner/banner.component';
     HeaderComponent,
     FooterComponent,
     BannerComponent,
+    AboutusComponent,
+    ServicesComponent,
+    BookserviceComponent,
   ],
   imports: [
     BrowserModule
---
+++ src/app/components/bookservice/bookservice.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-bookservice',
+  templateUrl: './bookservice.component.html',
+  styleUrls: ['./bookservice.component.css']
+})
+export class BookserviceComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
