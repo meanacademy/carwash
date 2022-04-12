ed373b0b0f343e335f4a7ba1cedb4bf35759c129
ravikant <ravikant.sontakke17@gmail.com>
2022-04-12 11:01:49 +0530

    fifth commit

--- .history/src/app/app.module_20220412105739.ts
+++ .history/src/app/app.module_20220412105739.ts
@@ -6,13 +6,26 @@ import { AppRoutingModule } from './app-routing.module';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { SpinnerComponent } from './spinner/spinner.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { BannerComponent } from './components/banner/banner.component';
+import { AboutusComponent } from './components/aboutus/aboutus.component';
+import { ServicesComponent } from './components/services/services.component';
+import { BookserviceComponent } from './components/bookservice/bookservice.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     FooterComponent,
+<<<<<<< HEAD
     SpinnerComponent
+=======
+    BannerComponent,
+    AboutusComponent,
+    ServicesComponent,
+    BookserviceComponent,
+>>>>>>> ff172e766232cf94200a64557dd2b84c3e6f350a
   ],
   imports: [
     BrowserModule,
